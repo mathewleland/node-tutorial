5743b66a0551c94bc97c87b169dfcaf31c66971b
mathewleland <mathewhleland@gmail.com>
2017-06-21 15:17:29 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## The wes bos learn node tutorial
+
+My own following along of Wes Bos' node tutorial
---
+++ routes/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+
+// Do work here
+router.get('/', (req, res) => {
+  const mathew = {
+    name: "mathew",
+    age: 29,
+    cool: true
+  }
+  //to access url queries like http://localhost:7777/?name=mathew&age=29
+  // res.send(req.query.name);
+  //or to format all of the queries as json
+  // res.json(req.query);
+  // res.json(mathew);
+  res.render('hello', {name: 'mathew', pet: req.query.pet || 'stevie'});
+});
+
+router.get('/reverse/:words', (req, res) => {
+  const reverse = [...req.params.words].reverse().join('');
+  res.send(reverse);
+})
+
+module.exports = router;
