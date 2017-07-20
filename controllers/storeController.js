64c17021f6e55ea98c8121fc0cd8d07786dff23d
mathewleland <mathewhleland@gmail.com>
2017-07-20 12:59:36 -0700

    start using middleware in the controller

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,13 @@
+exports.matsMiddleware = (req, res, next) => {
+  req.name = "matty";
+  res.cookie('name', req.name, {maxAge: 10000});
+  if (req.name === "matty") {
+    throw Error('Thats my name too')
+  }
+  next();
+}
+
 exports.homePage = (req, res) => {
-  res.render('index');
+  console.log(req.name)
+  res.render('tester');
 }
