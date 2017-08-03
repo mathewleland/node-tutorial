ea053e60920fec14967f1ebcc15f43f290ab59f6
mathewleland <mathewhleland@gmail.com>
2017-08-03 16:47:13 -0700

    start actually building store

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,11 +1,8 @@
-exports.matsMiddleware = (req, res, next) => {
-  req.name = "matty";
-  res.cookie('name', req.name, {maxAge: 10000});
-  if (req.name === "matty") {
-    throw Error('Thats my name too')
-  }
-  next();
-}
+//before we can create a store, we gotta describe what the data will look like.
+// like if we are making a spreadsheet, we'd first create the headers saying what kinds of data are going to be below.
+// mongodb can be a loose database (dont have to specify what the data will look like ahead of time).  but by default, out of the box, mongo is strict
+  // only can save data that it knows about beforehand.  if you add any kind of data, it can get out of hand and headaches appear when you scale.
+  //so we're doing everything in strict mode! so we define the schema first.
 
 exports.homePage = (req, res) => {
   console.log(req.name)
