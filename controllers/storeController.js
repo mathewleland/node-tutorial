1a1c79247eee807f7ad8f9aca78e8f2b435f211f
mathewleland <mathewhleland@gmail.com>
2017-08-12 22:05:52 -0700

    add store and create store created

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -4,11 +4,37 @@
   // only can save data that it knows about beforehand.  if you add any kind of data, it can get out of hand and headaches appear when you scale.
   //so we're doing everything in strict mode! so we define the schema first.
 
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store'); //comes from Store.js model, where it's exported
+
 exports.homePage = (req, res) => {
   console.log(req.name)
-  res.render('tester');
+  res.render('index');
 }
 
 exports.addStore = (req, res) => {
-  res.render('editStore', { title: Add Store });
+  res.render('editStore', { title:'Add Store' });
+}
+
+exports.createStore = async (req, res) => {
+  const store = new Store(req.body);
+  //what if someone passes us bad data in that req.body? it doesnt matter, in that store.js file,
+  // we are using a strict schema and setting our types there, anything else gets thrown away.
+  // you can keep adding fields:
+  // store.age = 1;
+  // store.cool = true;
+  //but nothing is reflected until you call:
+  await store.save();
+
+  console.log("that shit was successfully saved, lets go back to the home page");
+  res.redirect('/');
+  //javascript on its own will not wait to see if it saved, it will just go straight to redirecting you
+  // so we could put save on its own line, then chain some then(do this)
+  //.then(do that) .then(store => {
+  //   res.json(store);
+  // })
+  // .catch(err => {
+  //   throw Error(err);
+  // })
+  // in ES8, we can use async await instead
 }
