1dbb3775b49ac65c367890b0edf0a78e04b4edbe
mathewleland <mathewhleland@gmail.com>
2017-08-19 11:54:11 -0700

    add store now redirects you to that store's page after saving in await

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -21,18 +21,15 @@ exports.addStore = (req, res) => {
 }
 
 exports.createStore = async (req, res) => {
-  const store = new Store(req.body);
+  const store = await (new Store(req.body)).save();
   //what if someone passes us bad data in that req.body? it doesnt matter, in that store.js file,
   // we are using a strict schema and setting our types there, anything else gets thrown away.
   // you can keep adding fields:
   // store.age = 1;
   // store.cool = true;
   //but nothing is reflected until you call:
-  await store.save();
-
-  console.log("that shit was successfully saved, lets go back to the home page");
   req.flash('success', `flash worked! you just created a store named ${store.name}`);
-  res.redirect('/');
+  res.redirect(`/store/${store.slug}`);
   //javascript on its own will not wait to see if it saved, it will just go straight to redirecting you
   // so we could put save on its own line, then chain some then(do this)
   //.then(do that) .then(store => {
