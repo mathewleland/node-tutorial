f3e869d2597ee2ae3f18c54f59773a6a4ed49527
mathewleland <mathewhleland@gmail.com>
2017-09-28 16:02:02 -0700

    router post methods now accomodate image uploading, add in upload and resize middleware

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
-const { catchErrors } = require('../handlers/errorHandlers');
+const { catchErrors } = require('../handlers/errorHandlers'); //catch errors is for async functions
 
 router.get('/', catchErrors(storeController.getStores));
 router.get('/stores', catchErrors(storeController.getStores));
 router.get('/add', storeController.addStore);
-router.post('/add', catchErrors(storeController.createStore));
-router.post('/add/:id', catchErrors(storeController.updateStore));
+
+router.post('/add', 
+    storeController.upload, 
+    catchErrors(storeController.resize), 
+    catchErrors(storeController.createStore)
+);
+router.post('/add/:id', 
+    storeController.upload, 
+    catchErrors(storeController.resize),
+    catchErrors(storeController.updateStore)
+);
+
 router.get('/stores/:id/edit', catchErrors(storeController.editStore));
 
 module.exports = router;
