ef81e9622786cf227c4bbf999fb11189067a6756
mathewleland <mathewhleland@gmail.com>
2017-10-05 17:36:17 -0700

    initialize logout, isLoggedIn functions

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,3 +6,19 @@ exports.login = passport.authenticate('local', {
   successRedirect: '/',
   successFlash: 'You are now logged in bro! that\'s dope!'
 });
+
+exports.logout = (req, res) => {
+  req.logout();
+  req.flash('success', 'You are now logged out. Not dope.');
+  res.redirect('/');
+}
+
+exports.isLoggedIn = (req, res, next) => {
+  //first check if user is authenticated
+  if(req.isAuthenticated()) {
+    next(); // carry on! they are already logged in
+    return;
+  }
+  req.flash('error', 'you must be logged in to add stores');
+  res.redirect('/login');
+}
