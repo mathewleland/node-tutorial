b87c028f6477b314643ba548c9236ab4671b962a
mathewleland <mathewhleland@gmail.com>
2017-10-05 22:31:57 -0700

    typo fixed

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,24 +1,24 @@
 const passport = require('passport');
 
 exports.login = passport.authenticate('local', {
-  failureRedirect: '/login',
-  failureFlash: 'Failed Login!',
-  successRedirect: '/',
-  successFlash: 'You are now logged in bro! that\'s dope!'
+    failureRedirect: '/login',
+    failureFlash: 'Failed Login!',
+    successRedirect: '/',
+    successFlash: 'You are now logged in bro! that\'s dope!'
 });
 
 exports.logout = (req, res) => {
-  req.logout();
-  req.flash('success', 'You are now logged out. Not dope.');
-  res.redirect('/');
+    req.logout();
+    req.flash('success', 'You are now logged out. Not dope.');
+    res.redirect('/');
 }
 
 exports.isLoggedIn = (req, res, next) => {
   //first check if user is authenticated
-  if(req.isAuthenticated()) {
-    next(); // carry on! they are already logged in
-    return;
-  }
-  req.flash('error', 'you must be logged in to add stores');
-  res.redirect('/login');
-}
+    if(req.isAuthenticated()) {
+      next(); // carry on! they are already logged in
+      return;
+    }
+    req.flash('error', 'you must be logged in to add stores');
+    res.redirect('/login');
+};
