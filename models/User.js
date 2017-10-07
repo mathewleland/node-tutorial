08e0a035f3d1b7f05b0abaec98c19cbcc9562e7d
mathewleland <mathewhleland@gmail.com>
2017-10-06 22:11:39 -0700

    give user model password reset token and expiration properties

--- models/User.js
+++ models/User.js
@@ -19,7 +19,9 @@ const userSchema = new Schema({
       type: String,
       required: 'You need to have a name',
       trim: true
-  }
+  },
+  resetPasswordToken: String,
+  resetPasswordExpires: Date
 });
 
 userSchema.virtual('gravatar').get(function() {
