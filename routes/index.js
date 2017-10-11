161dae1b98bb4c9b2a92bac20b447ba96a58998d
mathewleland <mathewhleland@gmail.com>
2017-10-10 23:46:03 -0700

    create api endpoint route

--- routes/index.js
+++ routes/index.js
@@ -55,4 +55,8 @@ router.post('/account/reset/:token',
             catchErrors(authController.update)
 );
 
+//API ENDPOINTS
+
+router.get('/api/search', catchErrors(storeController.searchStores));
+
 module.exports = router;
