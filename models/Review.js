71a091fc1aea782f5b10b56f98d4c085e9327359
mathewleland <mathewhleland@gmail.com>
2017-10-13 21:32:52 -0700

    create review model

---
+++ models/Review.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+const reviewSchema = new mongoose.Schema({
+  created: {
+    type: Date,
+    defalt: Date.now()
+  },
+  author: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: 'You have to have an author!'
+  },
+  store: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Store',
+    required: 'A review has to be associated with a store!'
+  },
+  text: {
+    type: String,
+    required: 'Please give a brief review'
+  },
+  rating: {
+    type: Number,
+    min:1,
+    max:5
+  }
+});
+
+module.exports = mongoose.model('Review', reviewSchema);
