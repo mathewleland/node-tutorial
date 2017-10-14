4c8bbd6afd9e9b493b1dfd493347e32cc5d540c3
mathewleland <mathewhleland@gmail.com>
2017-10-13 21:33:08 -0700

    handle review posts

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const storeController = require('../controllers/storeController');
 const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
+const reviewController = require('../controllers/reviewController');
 const { catchErrors } = require('../handlers/errorHandlers'); //catch errors is for async functions
 
 router.get('/', catchErrors(storeController.getStores));
@@ -54,8 +55,15 @@ router.post('/account/reset/:token',
             authController.confirmedPasswords, 
             catchErrors(authController.update)
 );
+router.post('/reviews/:id', 
+    authController.isLoggedIn, 
+    catchErrors(reviewController.addReview)
+    );
 
-router.get('/hearts', authController.isLoggedIn, catchErrors(storeController.showHearts));
+router.get('/hearts', 
+    authController.isLoggedIn, 
+    catchErrors(storeController.showHearts)
+    );
 
 router.get('/map', storeController.mapPage);
 
